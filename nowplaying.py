017f289dc6bc9de08c73c50a6df9f34fca607bad
valerauko <valerauko@gmail.com>
2022-11-13 20:11:29 +0900

    Update because Quod Libet changed location

--- nowplaying.py
+++ nowplaying.py
@@ -10,7 +10,7 @@ from mastodon import Mastodon
 NS = 'org.mpris.MediaPlayer2.'
 
 bus = Bus(Bus.TYPE_SESSION)
-folder = os.path.dirname(__file__)+"/" # edit this if needed
+folder = os.path.dirname(__file__) + '/nowplaying/' # edit this if needed
 
 def try_player(name):
   try:
@@ -18,19 +18,48 @@ def try_player(name):
   except DBusException as e:
     return False
 
+# quod libet is above things so they don't use the mpris key
+def try_quod():
+  # if quod libet is not running then even just calling the get_object
+  # on its key will launch the app which is undesirable. so first check
+  # in the dbus name list if quod libet is running
+  list = bus.get_object('org.freedesktop.DBus', '/org/freedesktop/DBus').ListNames(
+    dbus_interface = 'org.freedesktop.DBus'
+  )
+  if 'net.sacredchao.QuodLibet' in list:
+    return bus.get_object(
+      'net.sacredchao.QuodLibet', '/net/sacredchao/QuodLibet'
+    )
+
 def get_player():
-  players = ['quodlibet', 'clementine', 'spotify']
+  players = ['clementine', 'spotify']
   for p in players:
     player = try_player(p)
     if player:
       return player
-  Exception('No player found')
 
-clem = get_player()
-data = clem.Get(NS + 'Player', 'Metadata', dbus_interface = 'org.freedesktop.DBus.Properties')
+data = None
+
+quod = try_quod()
+if quod is not None and quod.IsPlaying(dbus_interface = 'net.sacredchao.QuodLibet'):
+  quod_data = quod.CurrentSong(dbus_interface = 'net.sacredchao.QuodLibet')
+  # they even changed the format lol
+  data = {
+    'xesam:artist': [u'' + quod_data['artist']],
+    'xesam:album': u'' + quod_data['album'],
+    'xesam:title': u'' + quod_data['title']
+  }
+else:
+  player = get_player()
+  if player is None:
+    raise Exception('No player found')
+  data = player.Get(
+    NS + 'Player', 'Metadata',
+    dbus_interface = 'org.freedesktop.DBus.Properties'
+  )
 
 if not 'xesam:artist' in data.keys() or not 'xesam:title' in data.keys() or not 'xesam:album' in data.keys():
-  raise Exception('Wrong data format from player')
+  raise Exception('Unexpected data format from player')
 
 twt = u'#nowplaying ' +\
   ', '.join(data['xesam:artist']) +\
@@ -40,8 +69,6 @@ twt = u'#nowplaying ' +\
 
 # print twt
 # sys.exit()
-
-twt = twt.encode('utf8')
 
 app = open(folder + 'twitter_app.txt', 'r').read().splitlines()
 auth = tweepy.OAuthHandler(app[0], app[1])
