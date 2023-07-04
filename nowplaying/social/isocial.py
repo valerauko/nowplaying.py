8125461b7c981a345256c1fb306dd77a0cd49821
Vale <valerauko@gmail.com>
2023-07-04 18:45:48 +0900

    Add social interface

---
+++ nowplaying/social/isocial.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+class ISocial:
+    def __init__(self, config):
+        """Builds a social client based on config"""
+        pass
+
+    def post(self, text):
+        """Posts text to social media"""
+        pass
