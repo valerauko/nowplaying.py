6c5866ddacb357b5b0c6b54b6077492445ad781c
Vale <valerauko@gmail.com>
2023-07-04 19:26:28 +0900

    Re-export for ease of use

---
+++ nowplaying/player/__init__.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+
+from .clementine import Clementine
+from .quod import Quod
+from .spotify import Spotify
+
+__all__ = ['Clementine', 'Quod', 'Spotify']
---
+++ nowplaying/social/__init__.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+
+from .bluesky import Bluesky
+from .misskey import Misskey
+
+__all__ = ['Bluesky', 'Misskey']
