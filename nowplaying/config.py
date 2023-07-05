030f7b557a3f7317f306c77d6890c98c5e61247b
Vale <valerauko@gmail.com>
2023-07-06 02:26:34 +0900

    Python paths are weird

--- nowplaying/config.py
+++ nowplaying/config.py
@@ -1,7 +1,11 @@
 #coding=utf-8
 
+import os
 import yaml
 
 def read_config(path = 'config.yaml'):
-    with open(path, 'r') as stream:
+    file_path = os.path.realpath(
+        os.path.join(
+            os.path.dirname(__file__), '..', path))
+    with open(file_path, 'r') as stream:
         return yaml.safe_load(stream)
